c6a4e591d9dda261c1d37b07d23aadc3b9fef9e5
brainyadmin <admin@brainysystems.com>
2018-01-06 10:55:15 -0500

    First build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule }    from '@angular/http';
 import { AppComponent } from './app.component';
-import {OverlayPanelModule} from 'primeng/primeng';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,FormsModule,ReactiveFormsModule,HttpModule,OverlayPanelModule,BrowserAnimationsModule
+    BrowserModule,FormsModule,ReactiveFormsModule,HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
